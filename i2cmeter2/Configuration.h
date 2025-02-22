1749f9e2c375e2d69c8f98b3ece82396c0904531
Dennis <16547088+soligen2010@users.noreply.github.com>
2025-02-22 08:23:57 -0500

    Update Configuration.h

--- i2cmeter2/Configuration.h
+++ i2cmeter2/Configuration.h
@@ -9,7 +9,7 @@ WC8C, Dennis Cabell
 #define CONFIGURATION_H
 
 // comment this line out of you want to run hardware serial (wire to pin 0 instead of pin 9)
-#define USE_SW_SERIAL
+//#define USE_SW_SERIAL
 
 ///////////////////////////////////////////////////
 // To use a speed other than 9600, you must modify the Raduino CEC firmware and 
@@ -21,8 +21,8 @@ WC8C, Dennis Cabell
 // put baud=<rate> on the first line, for example:
 // baud=57600
 ///////////////////////////////////////////////////
-//#define SERIAL_SPEED 57600
-#define SERIAL_SPEED 9600
+#define SERIAL_SPEED 57600
+//#define SERIAL_SPEED 9600
 
 ///////////////////////////////
 // power swr configuration
@@ -34,22 +34,22 @@ WC8C, Dennis Cabell
 ///////////////////////////////
 // lower SMETER_GAIN to increase the S-Meter reading
 //////////////////////////////
-#define SMETER_GAIN         ((float)0.6)   // This should be close to the response of the original version with low resolution SMeter, no boost circuit - Your machine may vary
+//#define SMETER_GAIN         ((float)0.6)   // This should be close to the response of the original version with low resolution SMeter, no boost circuit - Your machine may vary
 //#define SMETER_GAIN         ((float)0.35)   // Value for WC8C's V4 board without boost circuit - Your machine may vary
-//#define SMETER_GAIN         ((float)1.6)   // Value for V4 board with WC8C SMeter boost circuit - Your machine may vary
+#define SMETER_GAIN         ((float)1.6)   // Value for V4 board with WC8C SMeter boost circuit - Your machine may vary
 
-//#define HIGH_RESOLUTION_SMETER           // Uncomment this if your Nextion has been changed to expect SMeter values from 0 to 80 instead of 0 to 8
+#define HIGH_RESOLUTION_SMETER           // Uncomment this if your Nextion has been changed to expect SMeter values from 0 to 80 instead of 0 to 8
 
 ///////////////////////////////
 // Power SWR meter calibration
 ///////////////////////////////
-#define CAL_REF_VOLTAGE (float)5.0             // Measured on multimeter
-#define CAL_FWD_DIODE_DROP  (float)0.24        // Measured on multimeter 
-#define CAL_REV_DIODE_DROP  (float)0.24        // Measured on multimeter
+//#define CAL_REF_VOLTAGE (float)5.0             // Measured on multimeter
+//#define CAL_FWD_DIODE_DROP  (float)0.24        // Measured on multimeter 
+//#define CAL_REV_DIODE_DROP  (float)0.24        // Measured on multimeter
 
-//#define CAL_REF_VOLTAGE (float)5.074            // WC8C's value Measured on multimeter
-//#define CAL_FWD_DIODE_DROP  (float)0.245        // WC8C's value Measured on multimeter (1N34A)
-//#define CAL_REV_DIODE_DROP  (float)0.235        // WC8C's value Measured on multimeter (1N34A)
+#define CAL_REF_VOLTAGE (float)5.074            // WC8C's value Measured on multimeter
+#define CAL_FWD_DIODE_DROP  (float)0.245        // WC8C's value Measured on multimeter (1N34A)
+#define CAL_REV_DIODE_DROP  (float)0.235        // WC8C's value Measured on multimeter (1N34A)
 
 #define DEFLT_CAL_FWD_ADJUST_MULTIPLIER (float)1.0    // set first by comparing forward power to a calibrated power meter
 
